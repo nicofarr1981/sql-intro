f738cbfe22e6abfd4de3f19c8460794a51ce0290
Nico Farr <nicofarr@gmail.com>
2022-04-08 20:42:29 +0000

    correction

--- lab-3-2.sql
+++ lab-3-2.sql
@@ -39,4 +39,7 @@
 -- | Washington Nationals          | 78.0             |
 -- +-------------------------------+------------------+
 
-select name, avg(wins) from teams where year > 2000 group by name;
+select name, round(avg(wins)) 
+from teams 
+where year > 2000 
+group by name;
